30c67e4dc0c9780ffd59603e36c8101e78346930
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-07-11 16:23:43 +0800

    Updated

--- app.js
+++ app.js
@@ -40,6 +40,37 @@ app.post('/create-user', function (req, res) {
 })
 
 /** 
+ * @description: Method that computes the sum, difference
+ * product, quotient
+ * @params: num1, num2
+ * @return answer
+ */
+ function getAnswers(num1, num2){
+
+    return {
+      product: num1 * num2,
+      difference: num1 - num2,
+      sum: num1 + num2,
+      quotient: num1 / 2
+    };
+ }
+
+ /** 
+ * @description: Method that computes the sum, difference
+ * product, quotient
+ * @params: num1, num2
+ * @return answer
+ */
+ function getOdd(num) {
+  for(var n in num) {
+    if(num.hasOwnProperty(n)) {
+      return JSON.stringify(num[n]);
+    }
+  }
+ }
+
+
+/** 
  * Create an API endpoint that will add/subtract/multiply/divide two numbers
  * Method: POST
  * Parameter:
@@ -49,6 +80,14 @@ app.post('/create-user', function (req, res) {
  *   answer
  *
  */
+app.post('/operation', function(req, res) {
+
+  var num1 = parseInt(req.body.num1);
+  var num2 = parseInt(req.body.num2);
+
+  return res.json(getAnswers(num1, num2));
+});
+
 
  /** 
  * Create an API endpoint that will only return odd or even numbers
@@ -67,6 +106,13 @@ app.post('/create-user', function (req, res) {
  * Response: [{"Number": "1"}, {"Number": "3"}]
  *
  */
+
+ app.post('/odd', function(req, res) {
+
+    console.log(typeof req.body.number);
+    console.log(getOdd(req.body.number));
+
+ });
 
  /** 
  * Create an API endpoint that will create a file with your name in it
